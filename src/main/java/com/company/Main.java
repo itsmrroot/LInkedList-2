c0c90a6953924f68952b2d184622e4cfab071225
Bashar <salmobeschir@gmail.com>
2026-07-27 11:56:23 +0200

    TESTING in main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,6 +4,18 @@ package com.company;
 public class Main {
    public static void main(String[] args) {
 
+       LinkedList l1 = new LinkedList();
+       Node newNode1 = new Node(22);
+       Node newNode2 = new Node(43);
+       Node newNode3 = new Node(5);
+       l1.createLinkedList();
+
+       l1.append(newNode1);
+       l1.insert(newNode2, 0);
+       l1.insert(newNode3, 2);
+       l1.delete(2);
+       l1.displayLinkedList();
+
 
 
 
