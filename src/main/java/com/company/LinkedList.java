1a08248fd0eb1e99f167097476b71b618c20cdd2
Bashar <salmobeschir@gmail.com>
2026-07-27 11:14:08 +0200

    ADD appened method

--- src/main/java/com/company/LinkedList.java
+++ src/main/java/com/company/LinkedList.java
@@ -7,6 +7,18 @@ public class LinkedList {
         this.head=null;
     }
 
+    public void append(int data){
+
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+        }
+        while (head.next!=null){
+            head=head.next;
+
+        }
+        head.next=newNode;
+    }
 
 
 
